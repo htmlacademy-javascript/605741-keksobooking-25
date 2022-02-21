fc4e9863eddb4370933aabc52dc60e95d8d8d77d
Валерия <sp.ether@yandex.ru>
2022-02-21 15:56:15 +0300

    Редактирует вспомогательные функции

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
 function getRandomIntInclusive(min, max) {
-  if (min >= max || min < 0 || max <= 0) {
-    return 'Задан неверный диапазон, необходимо выбрать другое значение';
-  }
-
   min = Math.ceil(min);
   max = Math.floor(max);
+
+  if (min >= max || min < 0 || max <= 0) {
+    throw new Error('Задан неверный диапазон, необходимо выбрать другое значение');
+  }
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 getRandomIntInclusive(5, 25);
 
 function getRandomIntInclusiveWithPoint(min, max, floatingPoint) {
-  if (min >= max || min < 0 || max <= 0) {
-    return 'Задан неверный диапазон, необходимо выбрать другое значение';
-  }
-
   min = Math.ceil(min);
   max = Math.floor(max);
-  const i = Math.random() * (max - min + 1) + min;
-  return i.toFixed(floatingPoint);
+
+  if (min >= max || min < 0 || max <= 0) {
+    throw new Error('Задан неверный диапазон, необходимо выбрать другое значение');
+  }
+
+  return (Math.random() * (max - min + 1) + min).toFixed(floatingPoint);
 }
 getRandomIntInclusiveWithPoint(6, 36, 4);
 
